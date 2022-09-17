5035941f0352f4a0f70116b5a52335aec1ac9cb4
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-09-17 15:02:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_image = document.getElementById('image');
+  element_image.setAttribute("src", 'https://drive.google.com/uc?export=view&id=12035OP-LBfooY9EpLO7oVcJL6ty6XU59');
 
+});
 
