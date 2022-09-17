1a8d346a9da3d6619f1235becf9b0191382aed9d
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-09-17 15:16:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('button').addEventListener('click', (event) => {
+document.getElementById('image').addEventListener('click', (event) => {
   let element_image = document.getElementById('image');
   element_image.setAttribute("src", 'https://static.remove.bg/remove-bg-web/c4b29bf4b97131238fda6316e24c9b3606c18000/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg');
 
