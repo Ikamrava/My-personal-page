d499986a5afe2ce40a97bcb68449f262ca7e49b0
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-09-24 14:22:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ dns.forEach((item) => {
   element_list.appendChild(new_li);
 });
 
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+document.getElementById('insert').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = getNumberOrString(document.getElementById('input').value);
+
+  element_list.appendChild(new_li);
+
+});
