aa9c0943ee92fa207a3474356da1c5d81d25fa4b
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-09-24 12:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ overlay.addEventListener("click", function () {
   document.querySelector(".modal").classList.add("hidden");
   document.querySelector(".overlay").classList.add("hidden");
 });
+
+
+
+var dns, link, item;
+dns = ['Code your future', 'Google', 'Git hub'];
+link = ['https://codeyourfuture.io/', 'https://www.google.com/', 'https://github.com/'];
+dns.forEach((item) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  let new_a = document.createElement('a');
+  new_a.innerText = item;
+  new_a.setAttribute("href", link.shift());
+
+  new_li.appendChild(new_a);
+
+  element_list.appendChild(new_li);
+});
+
