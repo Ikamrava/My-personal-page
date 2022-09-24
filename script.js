b069ea32bbf00b514a904e516cb5cd67d4cad12f
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-09-24 22:57:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const image_link = "https://cdn-icons-png.flaticon.com/512/1828/1828846.png";
+const close_image =
+  "https://uxwing.com/wp-content/themes/uxwing/download/checkmark-cross/close-round-icon.png";
 document
   .querySelector(".small_pic")
   .addEventListener("click", function (event) {
@@ -11,15 +14,17 @@ overlay.addEventListener("click", function () {
   document.querySelector(".overlay").classList.add("hidden");
 });
 
-
-
 var dns, link, item;
-dns = ['Code your future', 'Google', 'Git hub'];
-link = ['https://codeyourfuture.io/', 'https://www.google.com/', 'https://github.com/'];
+dns = ["Code your future", "Google", "Git hub"];
+link = [
+  "https://codeyourfuture.io/",
+  "https://www.google.com/",
+  "https://github.com/",
+];
 dns.forEach((item) => {
-  let element_list = document.getElementById('list');
-  let new_li = document.createElement('li');
-  let new_a = document.createElement('a');
+  let element_list = document.getElementById("list");
+  let new_li = document.createElement("li");
+  let new_a = document.createElement("a");
   new_a.innerText = item;
   new_a.setAttribute("href", link.shift());
 
@@ -32,17 +37,63 @@ function getNumberOrString(value) {
   // Convert a string value to a number if possible
   let number_value = Number(value);
   if (Number.isNaN(number_value)) {
-    return value
+    return value;
   } else {
-    return number_value
+    return number_value;
   }
 }
 
-document.getElementById('insert').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list2');
-  let new_li = document.createElement('li');
-  new_li.innerText = getNumberOrString(document.getElementById('input').value);
+document.getElementById("insert").addEventListener("click", (event) => {
+  let element_list = document.getElementById("list2");
+  let new_li = document.createElement("li");
+  new_li.innerText = getNumberOrString(document.getElementById("input").value);
 
   element_list.appendChild(new_li);
+});
 
+var value, item, links, http;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value;
+  } else {
+    return number_value;
+  }
+}
+
+value = getNumberOrString(document.getElementById("button_image").value);
+let element_button_image = document.getElementById("button_image");
+element_button_image.setAttribute("src", image_link);
+links = ["Google", "CYF", "Git hub"];
+http = [
+  "https://www.google.com/",
+  "https://codeyourfuture.io/",
+  "https://github.com/",
+];
+
+document.getElementById("button_image").addEventListener("click", (event) => {
+  if (value == "off") {
+    links.forEach((item) => {
+      let element_listoflinks = document.getElementById("listoflinks");
+      let new_li = document.createElement("li");
+      let new_a = document.createElement("a");
+      new_a.innerText = item;
+      new_a.setAttribute("href", http.shift());
+
+      new_li.appendChild(new_a);
+
+      element_listoflinks.appendChild(new_li);
+    });
+    value = "on";
+    let element_button_image2 = document.getElementById("button_image");
+    element_button_image2.setAttribute("src", close_image);
+  } else {
+    let element_listoflinks2 = document.getElementById("listoflinks");
+    element_listoflinks2.innerText = "";
+    value = "off";
+    let element_button_image3 = document.getElementById("button_image");
+    element_button_image3.setAttribute("src", image_link);
+  }
 });
