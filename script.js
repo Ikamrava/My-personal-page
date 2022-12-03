62d6803809e94900c3fd70ef869474461dcf0ced
ikamrava <ikamrava>
2022-12-03 18:31:43 +0000

    firebase

--- script.js
+++ script.js
@@ -115,6 +115,29 @@ document.getElementById("send").addEventListener("click", (event) => {
     document.getElementById("c_comment").value
   );
   new_div.setAttribute("class", "c_span");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC29hjAaM_pRZHfD1e1LliS_r13Fyj0SOQ",
+  authDomain: "my-first-projects-74a37.firebaseapp.com",
+  projectId: "my-first-projects-74a37",
+  storageBucket: "my-first-projects-74a37.appspot.com",
+  messagingSenderId: "532752384419",
+  appId: "1:532752384419:web:d973ef24577842467b9a16",
+  measurementId: "G-WKMKMJQ2KF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
   element_lname.appendChild(new_div);
 
